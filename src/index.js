cfa420e7888385b4e771668da27aa61cb06f9e65
Chris <cmlentz76@gmail.com>
2021-04-23 16:46:54 -0400

    testing git problem

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-/* basic fetch function */
-function fetchFunc() {
-  // To pass the tests, don't forget to return your fetch!
-  return fetch('https://website/api')
-  //  .then(resp => resp.json())
-  .then(function(resp) {
-    return resp.json();
-  })
+/* basic fetch function 
+https://xivapi.com/character/18533527 */
+function fetchChar() {
+  return fetch("https://xivapi.com/character/18533527")
+    .then(resp => resp.json())
+    .then(json => renderChar(json));
 }
+
+/*
+fetch("https://xivapi.com/character/18533527", { mode: 'cors' })
+	.then(response => response.json())
+	.then(data => console.info(data.Name_en))
+*/
