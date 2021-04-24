2d8c28c35f1cf9bb86b064ec7d42aadb0beab9d7
Chris <cmlentz76@gmail.com>
2021-04-24 05:58:55 -0400

    got first job and level from api into div

--- src/index.js
+++ src/index.js
@@ -7,4 +7,20 @@ function fetchChar() {
     
 }
 
-fetchChar().then(console.log);
+const createCharDiv = (char) => {
+  const charContainer = document.createElement('div')
+  const charClass = document.createElement('p')
+  const charLevel = document.createElement('p')
+
+  charClass.innerText = char.Character.ClassJobs[0].Name;
+  charLevel.innerText = char.Character.ClassJobs[0].Level;
+
+  charContainer.append(charClass, charLevel)
+
+  return charContainer
+}
+
+fetchChar().then((char) => {
+  const charDiv = createCharDiv(char)
+  console.log(charDiv)
+})
