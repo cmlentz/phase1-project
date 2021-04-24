f17dc1b789a7def69ec672a19669b8c70bd085e3
Chris <cmlentz76@gmail.com>
2021-04-24 15:08:17 -0400

    styling sizing spacing in css

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ function createCharDiv(char) {
   const charName = document.createElement('h2')
   const charPic = document.createElement('img')
   const charLevel = document.createElement('level-display')
+  const classDropdown = document.getElementById("class-dropdown");
 
   charName.innerText = char.Character.Name;
   charPic.src = char.Character.Portrait;
   
   charContainer.append(charName, charPic)
 
-  const classDropdown = document.getElementById("class-dropdown");
   for (let i = 0; i < char.Character.ClassJobs.length; i++) {
     const option = document.createElement("option");
     option.text = char.Character.ClassJobs[i].Name;
