dd124316a38913ce4c46cd5436aca3756a67f8ca
Chris <cmlentz76@gmail.com>
2021-04-24 14:04:04 -0400

    level displays after job is selected

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 My character id's
-
 Darkk Starr = https://xivapi.com/character/18533527
 Rockk Starr = https://xivapi.com/character/28677935
 Dwarff Starr = https://xivapi.com/character/31755207
+
--- src/index.js
+++ src/index.js
@@ -1,40 +1,36 @@
 function fetchChar() {
-  return fetch("https://xivapi.com/character/18533527")
+  return fetch("https://xivapi.com/character/28677935")
     .then(resp => resp.json())
 }
 
-const createCharDiv = (char) => {
+function createCharDiv(char) {
   const charContainer = document.createElement('div')
   const charName = document.createElement('h2')
   const charPic = document.createElement('img')
-  const charClassLevel = document.createElement('p')
-  // const charLevel = document.createElement('p')
+  const charLevel = document.createElement('level-display')
 
   charName.innerText = char.Character.Name;
   charPic.src = char.Character.Portrait;
-  charClassLevel.innerText = char.Character.ClassJobs[1].Name + ` = level ` + char.Character.ClassJobs[1].Level;
-  // charLevel.innerText = char.Character.ClassJobs[0].Level;
+  
+  charContainer.append(charName, charPic)
 
-  charContainer.append(charName, charPic, charClassLevel)
-
-  const dropdown = document.getElementById("dropdown");
+  const classDropdown = document.getElementById("class-dropdown");
   for (let i = 0; i < char.Character.ClassJobs.length; i++) {
     const option = document.createElement("option");
     option.text = char.Character.ClassJobs[i].Name;
-    option.value = char.Character.ClassJobs[i].Name;
-    dropdown.add(option);
+    option.value = `Level ` + char.Character.ClassJobs[i].Level;
+    classDropdown.add(option);
   }
-
+  
   return charContainer
 }
 
-const appendJob = (charDiv) => {
+function appendJob(charDiv) {
   const charContainer = document.getElementById('character-container')
-  charContainer.innerHTML = ""
   charContainer.append(charDiv)
 }
 
-const generateCharData =() => {
+function generateCharData () {
 fetchChar().then((char) => {
   const charDiv = createCharDiv(char)
   appendJob(charDiv)
