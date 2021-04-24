012d92bd4fa45a611325f0e9f8a10ea0e8731aab
Chris <cmlentz76@gmail.com>
2021-04-24 05:18:42 -0400

    fetch character data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Do stuff
+My character id's
+
+Darkk Starr = https://xivapi.com/character/18533527
+Rockk Starr = https://xivapi.com/character/28677935
+Dwarff Starr = https://xivapi.com/character/31755207
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-/* basic fetch function 
-https://xivapi.com/character/18533527 */
+// basic fetch function 
+
 function fetchChar() {
   return fetch("https://xivapi.com/character/18533527")
     .then(resp => resp.json())
-    .then(json => renderChar(json));
+    // .then(json => renderChar(json))
+    
 }
 
-/*
-fetch("https://xivapi.com/character/18533527", { mode: 'cors' })
-	.then(response => response.json())
-	.then(data => console.info(data.Name_en))
-*/
+fetchChar().then(console.log);
