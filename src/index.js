864439253068a672b98d2884524fac4c6aa89036
Chris <cmlentz76@gmail.com>
2021-04-24 09:32:39 -0400

    created dropdown selector and added jobs to it

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
-// basic fetch function 
-
 function fetchChar() {
   return fetch("https://xivapi.com/character/18533527")
     .then(resp => resp.json())
-    // .then(json => renderChar(json))
-    
 }
 
 const createCharDiv = (char) => {
   const charContainer = document.createElement('div')
-  const charClass = document.createElement('p')
-  const charLevel = document.createElement('p')
+  const charName = document.createElement('h2')
+  const charPic = document.createElement('img')
+  const charClassLevel = document.createElement('p')
+  // const charLevel = document.createElement('p')
 
-  charClass.innerText = char.Character.ClassJobs[0].Name;
-  charLevel.innerText = char.Character.ClassJobs[0].Level;
+  charName.innerText = char.Character.Name;
+  charPic.src = char.Character.Portrait;
+  charClassLevel.innerText = char.Character.ClassJobs[1].Name + ` = level ` + char.Character.ClassJobs[1].Level;
+  // charLevel.innerText = char.Character.ClassJobs[0].Level;
 
-  charContainer.append(charClass, charLevel)
+  charContainer.append(charName, charPic, charClassLevel)
+
+  const dropdown = document.getElementById("dropdown");
+  for (let i = 0; i < char.Character.ClassJobs.length; i++) {
+    const option = document.createElement("option");
+    option.text = char.Character.ClassJobs[i].Name;
+    option.value = char.Character.ClassJobs[i].Name;
+    dropdown.add(option);
+  }
 
   return charContainer
 }
 
 const appendJob = (charDiv) => {
   const charContainer = document.getElementById('character-container')
+  charContainer.innerHTML = ""
   charContainer.append(charDiv)
 }
 
+const generateCharData =() => {
 fetchChar().then((char) => {
   const charDiv = createCharDiv(char)
   appendJob(charDiv)
 })
+}
 
+generateCharData();
